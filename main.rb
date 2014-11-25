23ef4b43c686eb5971e55e18c12f89236a1b81b7
trsurber@belldouth.net <Tricia Surber>
2014-11-25 10:46:34 -0500

    personal site

--- main.rb
+++ main.rb
@@ -22,3 +22,40 @@ get '/works' do
     erb :works
 end
 
+#https://dev.twitter.com
+get '/tweets' do
+    require 'twitter'
+    OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+    client = Twitter::REST::Client.new do |config|
+        config.consumer_key = "h7tlbn9qdyA15VcRQjU5vG1yW"
+        config.consumer_secret = "fMl6MwHjVWQ6hO8mkvVjfbzMn6jZI4qcqsjUslOsIIcbQCYw7P"
+        config.access_token = "567667222-H7bZoZ05SvV3q0NKYYM6f34EDg4V4warm5OH7iak"
+        config.access_token_secret = "bDMsZqnmX6XTZYHIujTvOsajxRUfSwZCnXJzCqnXsbo3q"
+    end
+    
+         @search_results = client.search("@mercerfootball", result_type: "recent").take(30).collect do |tweet|
+      #"#{tweet.user.screen_name}: #{tweet.text}"
+        tweet
+    end
+    
+    @title = "My ____ Tweets"
+    @description = "This page contains my tweets that _____"
+    @tweets = "active"
+    erb :tweets
+end
+
+get '/news' do
+    require 'google-search'
+    query = "Mercer football"
+    #@results = "test"
+    @results = Array.new
+    Google::Search::News.new do |search|
+        search.query = query
+        search.size = :large
+    end.each { |item| @results.push item }
+    
+    @title = "New Stories"
+    @description = "News Stories About Mercer Football."
+    @news = "active"
+    erb :news
+end
